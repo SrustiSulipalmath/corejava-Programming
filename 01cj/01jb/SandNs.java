4fd4025ff9ccd9dd303241d9316b734467c7b52a
SrustiSulipalmath <97379669+SrustiSulipalmath@users.noreply.github.com>
2024-02-24 17:53:11 +0530

    Add files via upload

---
+++ 01cj/01jb/SandNs.java
@@ -0,0 +1,24 @@
+class A{
+	static int a = 10;
+	int x = 20;
+	
+	public static void main(String[] args){
+	System.out.println("main");
+
+	A a1 = new A();
+	A a2 = new A();
+	A a3 = new A();
+
+	System.out.println(a1.a);
+	System.out.println(a2.a);
+	System.out.println(a3.a);
+
+	System.out.println(a1.x);
+	System.out.println(a2.x);
+	System.out.println(a3.x);
+	
+	}
+}
+
+
+	
