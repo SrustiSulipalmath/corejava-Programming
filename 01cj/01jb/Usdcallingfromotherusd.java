2653960a44d01d3e3ac4cb4927df87905b3f4290
SrustiSulipalmath <97379669+SrustiSulipalmath@users.noreply.github.com>
2024-02-25 15:49:29 +0530

    Add files via upload

---
+++ 01cj/01jb/Usdcallingfromotherusd.java
@@ -0,0 +1,22 @@
+class Usdcallingfromotherusd{
+static void m1(){ 
+     
+    System.out.println("m1");
+}
+
+static void m2(){ 
+     
+    System.out.println("m2");
+    m1();
+}
+
+
+
+    
+public static void main(String[] args){
+    System.out.println("main");
+     m2();
+     
+}
+    
+}
