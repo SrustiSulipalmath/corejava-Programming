50e18660c502f7af9f2770fcdd8fdffe81a745a1
SrustiSulipalmath <97379669+SrustiSulipalmath@users.noreply.github.com>
2024-02-25 18:48:43 +0530

    Add files via upload

---
+++ 01cj/01jb/SameMethodInDiffClass.java
@@ -0,0 +1,29 @@
+class A{
+    static void m1(){
+        System.out.println(" A m1");
+    }
+    
+    void m2(){
+        System.out.println(" A m2");
+    }
+}
+
+class B{
+
+static void m1(){
+        System.out.println(" B m1");
+    }
+
+    public static void main(String[] args){
+        System.out.println("B main");
+            A.m1();
+            A a = new A();
+            a.m2();
+            B.m1();
+            m1();
+        
+        
+    
+    }
+    
+}
