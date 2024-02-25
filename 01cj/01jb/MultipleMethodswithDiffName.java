1ea8239fb49967fbb1987684c2eab49288221ae1
SrustiSulipalmath <97379669+SrustiSulipalmath@users.noreply.github.com>
2024-02-25 14:05:20 +0530

    Add files via upload

---
+++ 01cj/01jb/MultipleMethodswithDiffName.java
@@ -0,0 +1,19 @@
+class MultipleMethodswithDiffName{
+static void m1(){       
+    System.out.println("m1 ");
+}
+
+static void m2(){   
+    System.out.println("m2 ");
+}
+    
+public static void main(String[] args){
+    System.out.println("main");
+     m1();   
+     m2(); 
+     
+     m2(); 
+     m1(); 
+}
+    
+}
