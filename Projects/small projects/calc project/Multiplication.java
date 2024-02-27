18f223b4411365fc507233532af8493e17eb4c81
SrustiSulipalmath <97379669+SrustiSulipalmath@users.noreply.github.com>
2024-02-27 15:58:32 +0530

    Delete Projects/small projects/calc project directory

--- Projects/small projects/calc project/Multiplication.java
+++
@@ -1,7 +0,0 @@
-class Multiplication{
-
-    static void mul(int a, int b){
-        int c = a*b;
-        System.out.println("Multiplication of " +a+"and " +b+"is : "+ c);
-    }
-}
--- Projects/small projects/calc project/Subraction.java
+++
@@ -1,9 +0,0 @@
-class Subraction{
-
-    static void sub(int a,int b){
-        int c = a - b;
-        System.out.println("Subraction of " +a+"and " +b+"is : "+ c);
-    
-    
-	}
-}
