907a60f41909d45ef0749ee3886551b2c0b0b018
SrustiSulipalmath <97379669+SrustiSulipalmath@users.noreply.github.com>
2024-02-28 21:36:36 +0530

    Add files via upload

---
+++ 01cj/02packages/Example1.java
@@ -0,0 +1,6 @@
+package p1;
+class Example1{
+    public static void main(String[] args){
+        System.out.println("Ex1 main");
+    }
+}
---
+++ 01cj/02packages/Example2.java
@@ -0,0 +1,9 @@
+
+package p1;
+class Example2{
+    public static void main(String[] args)throws Exception{
+        System.out.println("main start");
+        Thread.sleep(10000L); //10000ms=10s
+        System.out.println("main end");
+    }
+}
