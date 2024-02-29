8d330e45d565c76029105ffb140f554bcc02e2a4
SrustiSulipalmath <97379669+SrustiSulipalmath@users.noreply.github.com>
2024-02-29 17:34:01 +0530

    Add files via upload

---
+++ 01cj/02packages/Example3.java
@@ -0,0 +1,16 @@
+package p1.p01;
+
+class Example3{
+    
+        public static void main(String[] args){
+            
+            System.out.println("Example3 Subpackage");
+        }
+
+}
+
+
+
+//tree - folder path listing
+//tree/f
+//dir
