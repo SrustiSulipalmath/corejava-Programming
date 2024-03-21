c562613bbea565c7914cf10335023c42adfa6465
SrustiSulipalmath <97379669+SrustiSulipalmath@users.noreply.github.com>
2024-03-21 16:42:59 +0530

    Add files via upload

---
+++ 01cj/02packages/case#2.2/p1/A1.java
@@ -0,0 +1,11 @@
+package p1;
+
+class A1{
+        static void m1(){
+            System.out.println("A1 m1");
+        }
+        
+         void m2(){
+            System.out.println("A1 m2");
+        }
+    }
