2851a7da61263e6cba3108f07129593b72b6d515
SrustiSulipalmath <97379669+SrustiSulipalmath@users.noreply.github.com>
2024-03-25 10:44:11 +0530

    Add files via upload

---
+++ 01cj/02packages/staticimport/Test1.java
@@ -0,0 +1,10 @@
+import static p1.A.a;
+class Test1{
+
+	public static void main(String[] args){
+	int a = 50;
+	System.out.println(a);
+	//System.out.println(A.a);
+	System.out.println(p1.A.a); // fully qualified name
+  }
+}
