dc1a4de907b46ace91369ca219923f2916461ad0
SrustiSulipalmath <97379669+SrustiSulipalmath@users.noreply.github.com>
2024-04-19 16:02:08 +0530

    Add files via upload

---
+++ Projects/small projects/commandprompt/calc project/Addition.java
@@ -0,0 +1,8 @@
+class Addition{
+
+    static void add(int a,int b){
+        int c = a+b;
+        System.out.println("Adittion of " +a+"and " +b+"is : "+ c);
+    
+	}
+}
---
+++ Projects/small projects/commandprompt/calc project/Division.java
@@ -0,0 +1,8 @@
+class Division{
+
+    static void div(int a, int b){
+        int c = a/b;
+	System.out.println("Division of " +a+"and " +b+"is : "+ c);
+    
+	}
+}
