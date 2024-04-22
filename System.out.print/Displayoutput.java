47a2a90b8611ed9d21fe7dd7697d6adeb1dd48f7
SrustiSulipalmath <97379669+SrustiSulipalmath@users.noreply.github.com>
2024-04-22 19:22:04 +0530

    Add files via upload

---
+++ System.out.print/Displayoutput.java
@@ -0,0 +1,24 @@
+class Displayoutput 
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("HK, ");
+		System.out.println("NiT");
+		System.out.print("Ameerpet, ");
+		System.out.println("Hyderabad");
+		System.out.println("Telangana");
+
+		System.out.print("HK, ");
+		System.out.print("NiT");
+		System.out.print("Ameerpet, ");
+		System.out.print("Hyderabad");
+		System.out.print("Telangana");
+
+		System.out.print("HK, ");
+		System.out.print("NiT\n");
+		System.out.print("Ameerpet, ");
+		System.out.print("Hyderabad\n");
+		System.out.print("Telangana");
+
+	}
+}
