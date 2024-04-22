480e510dad3cec3e8a3447d99c672a21bdd42725
SrustiSulipalmath <97379669+SrustiSulipalmath@users.noreply.github.com>
2024-04-22 17:56:43 +0530

    Add files via upload

---
+++ Projects/small projects/commandprompt/Multiplication.java
@@ -0,0 +1,8 @@
+
+public class Multiplication{
+
+    public static void mul(int a, int b){
+        int c = a*b;
+        System.out.println("Multiplication of " +a+"and " +b+"is : "+ c);
+    }
+}
